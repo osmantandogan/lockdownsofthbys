0336ed51d4b7073a6ebdbb44f15f7486b5f5a01a
Osman Şükrü Tandoğan <osmansukrutandogan@gmail.com>
2025-12-25 03:21:55 +0300

    Remove OneSignal service workers completely

--- frontend/public/OneSignalSDKWorker.js
+++
@@ -1,5 +0,0 @@
-// OneSignal Web Push Service Worker
-// Bu dosya OneSignal tarafından push bildirimleri almak için kullanılır
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-
